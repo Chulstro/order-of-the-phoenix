ee0955b47ed55cf313292b678c921e164f20c6ff
Chandler Hulstrom <chulstro11@gmail.com>
2020-09-01 10:30:44 -0600

    refactored controller and endpoint

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,8 @@
 class SearchController < ApplicationController
   def index
     conn = Faraday.new(url: 'https://www.potterapi.com')
-    response = conn.get("/v1/characters?key=#{ENV['POTTER_KEY']}&house=#{params[:house]}&orderOfThePhoenix=true")
+    query = "key=#{ENV['POTTER_KEY']}&house=#{params[:house]}&orderOfThePhoenix=true"
+    response = conn.get("/v1/characters?#{query}")
     @students = JSON.parse(response.body)
   end
 end
