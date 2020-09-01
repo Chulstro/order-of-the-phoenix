4c5b562a09e3f13b8b75cce2a0f6f19028af80f9
Chandler Hulstrom <chulstro11@gmail.com>
2020-09-01 09:57:51 -0600

    test for search complete

---
+++ spec/features/search_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe "When searching as a user" do
+  it 'Lets you search by house' do
+    visit '/'
+
+    select('Gryffindor', from: :house)
+
+    click_on "Search For Members"
+
+    expect(page).to have_content("Aberforth Dumbledore")
+    expect(page).to have_content("Gryffindor")
+    expect(page).to have_content("Owner, Hog's Head Inn")
+    expect(page).to have_content("Patronus: goat")
+  end
+end
